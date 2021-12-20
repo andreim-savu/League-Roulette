21b272b53982c0682279c9f7bb1bc8dd5bd575f6
Andrei Savu <savu.andrei96@outlook.com>
2021-12-20 05:40:14 +0200

    1

---
+++ LeagueRoulette/src/app/app.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'LeagueRoulette';
+
+  playersArr: FormArray = new FormArray([]);
+
+  players = [];
+
+  ngOnInit(): void {
+    this.addPlayer();
+    console.log((this.playersArr.controls[0] as FormGroup).controls["name"].value);
+  }
+
+  addPlayer(): void {
+    let newPlayer = new FormGroup({
+      name: new FormControl("", [Validators.required])
+    });
+    this.playersArr.push(newPlayer);
+  }
+
+  calculateRoles(): void {
+    let roles = ["Top", "Jungle", "Mid", "Bottom", "Supp"];
+
+    this.players = [];
+    for (let group of this.playersArr.controls) {
+      let player = {
+        name: (group as FormGroup).controls["name"].value,
+        order: []
+      };
+      
+      this.players.push(player);
+    }
+
+    let runs = 0;
+
+    while (runs < roles.length) {
+      let orderOk = true;
+      let tempRoles = [...roles];
+      
+      for (let j = 0; j < 100; j++) {
+        let index1 = Math.floor(Math.random() * tempRoles.length);
+        let index2 = Math.floor(Math.random() * tempRoles.length);
+
+        if (index1 === index2) { continue; }
+
+        let tempRole = tempRoles[index1];
+        tempRoles[index1] = tempRoles[index2];
+        tempRoles[index2] = tempRole;
+      }
+
+      for (let j = 0; j < this.players.length; j++) {
+          if (this.players[j].order.includes(tempRoles[j])) { orderOk = false; }
+      }
+  
+      if (orderOk) {
+          for (let j = 0; j < this.players.length; j++) {
+            this.players[j].order.push(tempRoles[j]);
+          }
+          runs++;
+          console.log(runs);
+      }
+    }
+  }
+}
